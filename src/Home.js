8d80176f02a25677916b35b9c48a5660ba57c65c
roy <aroy.sourov@gmail.com>
2021-12-28 18:52:00 +0600

    init

---
+++ src/Home.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './Home.css';
+import logo from "./img/logo.png"
+
+function Home() {
+    return (
+        <div className="home">
+           <div className="home__bg">
+
+               <div className="header d__flex align__items__center pxy_30">
+                   
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                    </div>
+
+                    <div className="navigation pxy__30">
+                       <ul>
+                       <div className="navbar d__flex">
+                            <a href="#Home"><li className="nav__items mx__15">Home</li></a>
+                            <a href="#About"><li className="nav__items mx__15">About</li></a>
+                            <a href="#Services"><li className="nav__items mx__15">Services</li></a>
+                            <a href="#Portfolio"><li className="nav__items mx__15">Portfolio</li></a>
+                            <a href="#Blog"><li className="nav__items mx__15">Blog</li></a>
+                            <a href="#Contact"><li className="nav__items mx__15">Contact</li></a>
+                       </div>
+                       </ul>
+                    </div>
+               </div>
+                   {/* Home Content */}
+
+               <div className="container">
+                    <div className="home__content">
+                        <div className="home__meta">
+
+                            <h1 className="home__text pz__10">
+                                Wellcome To My World
+                            </h1>
+
+                            <h2 className="home__text pz__10">
+                                    Hi I'm Roy
+                            </h2>
+
+                            <h3 className="home__text sweet">
+                                Js Developer
+                            </h3>
+
+                            <h4 className="home__text pz__10">
+                                based in USA
+                            </h4>
+
+
+                        </div>
+                    </div>
+               </div>
+
+           </div>
+        </div>
+    )
+}
+
+export default Home
