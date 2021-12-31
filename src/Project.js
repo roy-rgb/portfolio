ac6731e4a1ec405b42f0f45e00bcdd82cd15aeca
roy <aroy.sourov@gmail.com>
2021-12-31 11:45:58 +0600

    project done

--- src/Project.js
+++ src/Project.js
@@ -4,7 +4,6 @@ import Project1 from "./img/portfolio-1.jpg";
 import Project2 from "./img/portfolio-2.jpg";
 import Project3 from "./img/portfolio-3.jpg";
 
-
 function Project() {
   return (
     <div className="project component__space">
@@ -18,112 +17,132 @@ function Project() {
         </p>
       </div>
 
-        <div className="container">
-                <div className="row">
-
-                    <div className="col__3">
-                        <div className="project__box  pointer relative">
-                            <div className="project__box__img pointer relative">
-                                <div className="project__img__box">
-                                    <img src={Project1} alt="" className="project__img" />
-                                </div>
-                                <div className="mask__effect"></div>
-                            </div>
-                            <div className="project__meta absolute">
-                              <h5 className="project__text">Development</h5>
-                              <h4 className="project__text">Getting tickets to the big show</h4>
-                                <a href="#" className="project__btn">View Details</a>                     
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div className="col__3">
-                        <div className="project__box  pointer relative">
-                            <div className="project__box__img pointer relative">
-                                <div className="project__img__box">
-                                    <img src={Project2} alt="" className="project__img" />
-                                </div>
-                                <div className="mask__effect"></div>
-                            </div>
-                            <div className="project__meta absolute">
-                              <h5 className="project__text">Development</h5>
-                              <h4 className="project__text">Getting tickets to the big show</h4>
-                                <a href="#" className="project__btn">View Details</a>                     
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col__3">
-                        <div className="project__box  pointer relative">
-                            <div className="project__box__img pointer relative">
-                                <div className="project__img__box">
-                                    <img src={Project3} alt="" className="project__img" />
-                                </div>
-                                <div className="mask__effect"></div>
-                            </div>
-                            <div className="project__meta absolute">
-                              <h5 className="project__text">Development</h5>
-                              <h4 className="project__text">Getting tickets to the big show</h4>
-                                <a href="#" className="project__btn">View Details</a>                     
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col__3">
-                        <div className="project__box  pointer relative">
-                            <div className="project__box__img pointer relative">
-                                <div className="project__img__box">
-                                    <img src={Project3} alt="" className="project__img" />
-                                </div>
-                                <div className="mask__effect"></div>
-                            </div>
-                            <div className="project__meta absolute">
-                              <h5 className="project__text">Development</h5>
-                              <h4 className="project__text">Getting tickets to the big show</h4>
-                                <a href="#" className="project__btn">View Details</a>                     
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col__3">
-                        <div className="project__box  pointer relative">
-                            <div className="project__box__img pointer relative">
-                                <div className="project__img__box">
-                                    <img src={Project1} alt="" className="project__img" />
-                                </div>
-                                <div className="mask__effect"></div>
-                            </div>
-                            <div className="project__meta absolute">
-                              <h5 className="project__text">Development</h5>
-                              <h4 className="project__text">Getting tickets to the big show</h4>
-                                <a href="#" className="project__btn">View Details</a>                     
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col__3">
-                        <div className="project__box  pointer relative">
-                            <div className="project__box__img pointer relative">
-                                <div className="project__img__box">
-                                    <img src={Project2} alt="" className="project__img" />
-                                </div>
-                                <div className="mask__effect"></div>
-                            </div>
-                            <div className="project__meta absolute">
-                              <h5 className="project__text">Development</h5>
-                              <h4 className="project__text">Getting tickets to the big show</h4>
-                                <a href="#" className="project__btn">View Details</a>                     
-                            </div>
-                        </div>
-                    </div>
-
-
+      <div className="container">
+        <div className="row">
+          <div className="col__3">
+            <div className="project__box  pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project1} alt="" className="project__img" />
                 </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">
+                  Getting tickets to the big show
+                </h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
 
+          <div className="col__3">
+            <div className="project__box  pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project2} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">
+                  Getting tickets to the big show
+                </h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="project__box  pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project3} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">
+                  Getting tickets to the big show
+                </h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="project__box  pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project3} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">
+                  Getting tickets to the big show
+                </h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="project__box  pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project1} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">
+                  Getting tickets to the big show
+                </h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="project__box  pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project2} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">
+                  Getting tickets to the big show
+                </h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
+              <div className="view__more__btn d__flex align__items__center justify__content__center pxy__30">
+                  <button className="view__more pointer btn">View more</button>
+                </div>  
         </div>
-
-
+      </div>
     </div>
   );
 }
